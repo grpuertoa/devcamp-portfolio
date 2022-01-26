98a4583bc508214375cca2983c5e3ce20ff4dc5c
grpuertoa <87292166+grpuertoa@users.noreply.github.com>
2022-01-26 09:49:28 -0500

    Added portfolio via resource generator

---
+++ db/migrate/20220126144434_create_portfolios.rb
@@ -0,0 +1,13 @@
+class CreatePortfolios < ActiveRecord::Migration[7.0]
+  def change
+    create_table :portfolios do |t|
+      t.string :titile
+      t.string :subtitile
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
